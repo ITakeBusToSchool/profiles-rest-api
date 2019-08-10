5d48316b83bbb9d3a240ddea9c714d6f7f814bc7
Apple <apple@Applede-MBP.fios-router.home>
2019-08-10 15:14:10 -0400

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from profiles_api import serializers
+from rest_framework.authentication import TokenAuthentication
 
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -46,6 +49,7 @@ class HelloApiView(APIView):
 
     def delete(self,request, pk=None):
         return Response({'method':'DELETE'})
+
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViestSet"""
 
@@ -91,3 +95,10 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self,request, pk=None):
         """Handle removing an object"""
         return Response({'http_method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """handle creating and update profiles"""
+    serializer_class=serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permissioon_classes = (permissions.UpdateOwnProfile,)
